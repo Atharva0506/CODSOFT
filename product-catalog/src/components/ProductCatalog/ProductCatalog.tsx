6e3c32ac0a168316f07e54540288718bcb6fdbd2
Atharva Naik <atharvan.coder@gmail.com>
2023-12-28 20:52:45 +0530

    Task3 crating filters

--- product-catalog/src/components/ProductCatalog/ProductCatalog.tsx
+++ product-catalog/src/components/ProductCatalog/ProductCatalog.tsx
@@ -1,30 +1,76 @@
 "use client"
-import React from 'react';
-import products from '@/product';
+import React, { useState } from 'react';
+import products from '@/product.json';
 import Image from 'next/image';
 import "./ProductCatalog.css"
+
+export interface ProductCatalog{
+    id: number,
+    name: string,
+    price: number,
+    image: string,
+    category: string  
+}
 function ProductCatalog() {
+  const [productData,setProductData] = useState<ProductCatalog[]>(products);
+  const [categoryData,setCategoryData]= useState<any>([]);
+
+   const filterData = (data: ProductCatalog[], field: string) => {
+    let filteredData = data.map((currentElem: any) => {
+      return currentElem[field]; 
+    });
+  return (filteredData=[...new Set(filteredData)]) 
+  }
+  
+  const categoryType = filterData(products, "category"); 
+  const rangeType = filterData(products, "range")
+  const handleCategoryCheck = (event:any)=>{
+  const category = event.target.value;
+    if(event.target.checked){
+      setCategoryData([...categoryData,category])
+      console.log(categoryData);
+    }else{
+      setCategoryData(categoryData.filter((catg:any)=>{catg!==category}))
+    }
+  }
+console.log(categoryData)
   return (
       <section id='Products'>
       <h2>Products</h2>
+      <div className="catalog_nav">
+        <ul>
+       {categoryType.map((item,index)=>(
+        <li key={index}>
+            <div>
+              <input type="checkbox" 
+              value={item}
+              checked={categoryData.includes(item)}
+              onChange={()=>{handleCategoryCheck}}
+              />
+            </div>
+            <div style={{color : 'white'}}>{item}</div>
+        </li>
+       ))}
+      </ul>
+      </div>
       <div className="container products_container">
       {
-        products.map((product)=>{
-          return (
-            <article key={product.id} className='products_item'>
-            <div className='products_img'>
-              <img src={product.image} alt="products_img" />
-            </div>
+        // products.map((product)=>{
+        //   return (
+        //     <article key={product.id}>
+        //     <div className='products_img'>
+        //       <img src={product.image} alt="products_img" />
+        //     </div>
 
-            <h3>{product.name}&nbsp; {product.price}</h3>
+        //     <h3>{product.name}&nbsp; {product.price}</h3>
             
-            <div className='products_item-cta'>
-              <button  className='btn' >Add to Cart</button>
-              <button  className='btn btn-primary'>Vist</button>
-            </div>
-          </article>
-          )
-        })
+        //     <div className='products_item-cta'>
+        //       <button  >Add to Cart</button>
+        //       <button  >Vist</button>
+        //     </div>
+        //   </article>
+        //   )
+        // })
       }
 
       </div>
